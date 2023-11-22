0652fc1410031b33da56a6b436b71195b28b6a51
Secrecion <secrecion@gmail.com>
2023-11-21 20:16:27 -0600

    update socket and cookie parser

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ const auth = require("./auth.js");
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo=require('passport.socketio');
+const MongoStore=require('connect-mongo')(session);
+const cookieParser= require('cookie-parser');
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
+
 
 
 
@@ -25,6 +32,8 @@ app.use(
     resave: true,
     saveUninitialized: true,
     cookie: { secure: false },
+    key: 'express.sid',
+  store: store
   })
 );
 
@@ -35,6 +44,17 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 // app.route('/').get((req, res) => {
 //   res.render('index', { title: 'Hello', message: 'Please log in' });
@@ -50,11 +70,13 @@ myDB(async (client) => {
     ++currentUsers;
     io.emit('user count', currentUsers);
     console.log('A user has connected');
+
     socket.on('disconnect', () => {
-      console.log('A user has disconnected');
+      console.log('user ' + socket.request.user.username + ' connected');
       --currentUsers;
       io.emit('user count', currentUsers);
     });
+
   });
 
 }).catch((e) => {
@@ -63,6 +85,19 @@ myDB(async (client) => {
   });
 });
 // app.listen out here...
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
 
 
 
