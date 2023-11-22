600a9a09c4c83d30a4202f2be119b67fad1af044
Secrecion <secrecion@gmail.com>
2023-11-21 18:59:44 -0600

    Update for io and http

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const LocalStrategy = require("passport-local");
 const bcrypt = require("bcrypt");
 const routes = require("./routes.js");
 const auth = require("./auth.js");
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 
 const app = express();
 app.set("view engine", "pug");
@@ -41,6 +44,9 @@ myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
   routes(app, myDataBase);
   auth(app, myDataBase);
+  io.on('connection', socket => {
+    console.log('A user has connected');
+  });
 
 }).catch((e) => {
   app.route("/").get((req, res) => {
@@ -52,6 +58,6 @@ myDB(async (client) => {
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log("Listening on port " + PORT);
 });
